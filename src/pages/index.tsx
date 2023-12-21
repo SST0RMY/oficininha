67d57d419d647750326ce73c796508ad5d810c82
SST0RMI <a85220@ualg.pt>
2023-12-21 18:18:17 +0000

    teste2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 58min ⏱️
+            Docusaurus Tutorial - 581min ⏱️
           </Link>
         </div>
       </div>
